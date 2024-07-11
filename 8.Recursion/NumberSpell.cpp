af6daefd7194811d1391780a0d05314a339561a4
SinhaWiz <151841974+SinhaWiz@users.noreply.github.com>
2024-07-11 11:03:35 +0600

    Update NumberSpell.cpp

--- 8.Recursion/NumberSpell.cpp
+++ 8.Recursion/NumberSpell.cpp
@@ -2,14 +2,12 @@
 using namespace std ;
 string Numbers[] = {"Zero ", "One ", "Two ","Three ","Four ","Five ","Six ","Seven ","Eight ","Nine "};
 void SpellNumber(int n){
-
     if(n==0){
         return ;
     }
     int Last_Digit  = n%10;
     SpellNumber(n/10);
     cout<<Numbers[Last_Digit];
-
 }
 int main(){
 int n ;
